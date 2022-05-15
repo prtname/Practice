16e90b0688cbf58145af346c489eeaeab9eff8a2
prtname <91187493+prtname@users.noreply.github.com>
2022-05-15 16:24:31 +0300

    Add User, Phone, UserRole models

---
+++ Models/Phone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Models
+{
+    public class Phone
+    {
+        public Phone(int id, char countryCode, char[] number)
+            : this(countryCode, number)
+        {
+            m_id = id;
+        }
+
+        public Phone(char countryCode, char[] number)
+        {
+            m_countryCode = countryCode;
+            m_number = number;
+        }
+
+
+
+
+        public int Id => m_id;
+        public char CountryCode => m_countryCode;
+        public char[] Number => m_number;
+
+
+
+        private int m_id;
+        private char m_countryCode;
+        private char[] m_number;
+    }
+}
