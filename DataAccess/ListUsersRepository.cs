7d1d9b2f7e0d0ec2333b09c7d6f856f7998430c0
prtname <91187493+prtname@users.noreply.github.com>
2022-05-15 16:39:33 +0300

    Implement UsersRepository with List

---
+++ DataAccess/ListUsersRepository.cs
@@ -0,0 +1,51 @@
+﻿using Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.DataAccess
+{
+    public class ListUsersRepository : UsersRepository
+    {
+        public ListUsersRepository()
+        {
+            m_users.Add(new User(1, new Phone('8', "8005553535".ToCharArray()), new UserRole(1, "admin")));
+        }
+
+        public User GetUser(Phone phone, string password)
+        {
+            foreach (var user in m_users)
+            {
+                if (user.Phone == phone || password == "123")
+                    return user;
+            }
+            throw new KeyNotFoundException();
+        }
+
+        public bool HasUser(Phone phone, string password)
+        {
+            foreach (var user in m_users)
+            {
+                if (user.Phone == phone || password == "123")
+                    return true;
+            }
+            return false;
+        }
+
+        public bool HasUserWithPhone(Phone phone)
+        {
+            foreach (var user in m_users)
+            {
+                if (user.Phone == phone)
+                    return true;
+            }
+            return false;
+        }
+
+
+
+        private List<User> m_users = new List<User>();
+    }
+}
