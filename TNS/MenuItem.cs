ad424fab7f852f19ef11cb9bc59649acffd3c25e
prtname <91187493+prtname@users.noreply.github.com>
2022-05-17 21:31:29 +0300

    Implement MenuItem

--- TNS/MenuItem.cs
+++ TNS/MenuItem.cs
@@ -8,5 +8,13 @@ namespace TNS
 {
     public class MenuItem
     {
+        public MenuItem(string title, Uri uri)
+        {
+            Title = title;
+            Uri = uri;
+        }
+
+        public string Title { get; set; }
+        public Uri Uri { get; set; }
     }
 }
